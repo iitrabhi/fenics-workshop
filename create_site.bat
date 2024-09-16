a3435291b92e87a3fd18c837eda52f67814e50de
Ashvin Oli <ashvinoli10@gmail.com>
2024-09-15 19:47:48 -0500

    Indentation issue fixed and script to create site uploaded

---
+++ create_site.bat
@@ -0,0 +1,14 @@
+@echo off
+echo removing old files..
+rm -r docs/
+mkdir docs
+
+echo building new...
+mkdocs build --config-file mkdocs/mkdocs.yml
+
+echo copying files..
+cp -r mkdocs/site/** docs
+
+echo opening site...
+"docs/index.html"
+echo done!
